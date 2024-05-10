24f5d73425f46a569ce32519b58922bcd1b07091
dfmc0 <12550033@ieselgrao.org>
2024-05-10 16:59:38 +0200

    Cosas

---
+++ MiProyecto/src/cosa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DAW6W
+ */
+public class cosa {
+    public static void main(String[] args) {
+        System.out.println("COSA");
+    }
+}
