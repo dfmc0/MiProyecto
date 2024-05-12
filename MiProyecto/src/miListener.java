9d3c8fdeead9b766bff1057a44bcf514b2165012
dfmc0 <12550033@ieselgrao.org>
2024-05-12 18:27:56 +0200

    Casi fin

---
+++ MiProyecto/src/miListener.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+
+/**
+ *
+ * @author DAW6W
+ */
+public class miListener implements ActionListener{
+    String formato;
+    public miListener(String f){
+        formato = f;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JRadioButton boton= (JRadioButton)e.getSource();
+        formato = boton.getText();
+    }
+    public String getFor(){
+        return formato;
+    }
+    
+}
